c21eebe23175c52e7641532b5137ed3b5e4ea011
nmhung@cmc.com.vn <nmhung@cmc.com.vn>
2021-06-13 18:50:01 +0700

    fix sonarlint ErrMessage

--- src/main/java/com/nmhung/organization/exceptions/ErrorMessage.java
+++ src/main/java/com/nmhung/organization/exceptions/ErrorMessage.java
@@ -3,4 +3,6 @@ package com.nmhung.organization.exceptions;
 public class ErrorMessage {
     public static final String ORG_NOT_FOUND = "Organization not found!";
     public static final String DUPLICATE_ORG_NAME = "Organization has this name existed!";
+
+    private ErrorMessage(){}
 }
